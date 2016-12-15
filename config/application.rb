dde7b449f7e8a256494823613a3f7cad405ddbbe
Bill Wiegert <billsteh@gmail.com>
2016-12-15 18:14:29 -0500

    Update config for AWS

--- config/application.rb
+++ config/application.rb
@@ -31,11 +31,11 @@ module Constructibles
 
     config.paperclip_defaults = {
       storage: :s3,
+        s3_region: ENV["s3_region"],
       s3_credentials: {
         bucket: ENV["s3_bucket"],
         access_key_id: ENV["s3_access_key_id"],
         secret_access_key: ENV["s3_secret_access_key"],
-        s3_region: ENV["s3_region"]
       }
     }
 
